0c28f6b25965262bdf2adbc91b5dc7c75fa3e782
Ken Zhou <kendotzhou@gmail.com>
2022-04-18 18:29:22 -0400

    set up fish

--- fish/install.sh
+++ fish/install.sh
@@ -2,3 +2,11 @@
 echo /usr/bin/fish | sudo tee -a /etc/shells
 # change default shell to fish
 chsh -s /usr/bin/fish
+
+# install oh my fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+
+# set as executable
+chmod +x ../omf/install.sh
+# install things for omf
+../omf/install.sh
---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+sudo apt install fish
+# set as executable
+chmod +x ./fish/install.sh
+./fish/install.sh
