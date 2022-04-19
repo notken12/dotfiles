70f7d64bc30d75508b697649f63a0665c542ce90
Ken Zhou <kendotzhou@gmail.com>
2022-04-19 09:29:38 -0400

    stop adding brew over and over to path

--- fish/config.fish
+++ fish/config.fish
@@ -3,9 +3,9 @@ alias v="nvim"
 # echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/notken12/.profile
     # eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
-set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
+# set -U fish_user_paths /opt/homebrew/bin $fish_user_paths
 
-set -U fish_user_paths /home/linuxbrew/.linuxbrew/bin/brew $fish_user_paths
+# set -U fish_user_paths /home/linuxbrew/.linuxbrew/bin/brew $fish_user_paths
 
 set default_path /usr/bin /usr/sbin /bin /sbin
 set homebrew /home/linuxbrew/.linuxbrew/bin 
@@ -15,3 +15,4 @@ set -gx PATH $homebrew $brew_rbenv $default_path
 theme_gruvbox dark medium
 
 nvm use --lts --silent
+eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
